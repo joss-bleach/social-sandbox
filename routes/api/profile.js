cf7a7dc0c1c4f004d2dd93bb1ddfec6cccda2827
Web <web@Josss-MacBook-Pro.local>
2020-05-08 21:33:51 +0100

    Added functionality to get GitHub Repos.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,8 @@ const auth = require("../../middleware/auth");
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
 const { check, validationResult } = require("express-validator");
+const request = require("request");
+const config = require("config");
 
 //ROUTE - GET /api/profile/me
 //DESC - Get current user profile
@@ -281,6 +283,33 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
     profile.education.splice(remove_index, 1);
     await profile.save();
     res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error.");
+  }
+});
+
+//ROUTE - GET /api/profile/github/:username
+//DESC - Get user repos from GitHub
+//ACCESS - Public
+router.get("/github/:username", (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created: asc&client_id=${config.get(
+        "githubClientId"
+      )}&client_secret=${config.get("githubClientSecret")}`,
+      method: "GET",
+      headers: { "user-agent": "node.js" },
+    };
+    request(options, (error, response, body) => {
+      if (error) console.error();
+      if (response.statusCode !== 200) {
+        res.status(404).json({ msg: "No GitHub profile found." });
+      }
+      res.json(JSON.parse(body));
+    });
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error.");
