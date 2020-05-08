bce91ef5b7b15a24d50571e1e2ee7af636af7147
Web <web@Josss-MacBook-Pro.local>
2020-05-08 17:10:54 +0100

    Authorisation middleware added

---
+++ middleware/auth.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = function (req, res, next) {
+  const token = req.header("x-auth-token");
+  if (!token) {
+    return res.status(401).json({ msg: "Unauthorised Request." });
+  }
+
+  try {
+    const decoded = jwt.verify(token, config.get("secretOrKey"));
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Unauthorised Requrst" });
+  }
+};
