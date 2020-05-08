2a3f7e125a7ef35d1e3e1cbd5aa1c5cac95f348d
Web <web@Josss-MacBook-Pro.local>
2020-05-08 18:36:05 +0100

    Get current profile added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
-//ROUTE - GET /api/profile
-//DESC - Test Route
-//ACCESS - Public
-router.get("/", (req, res) => res.send("Profile Route"));
+//ROUTE - GET /api/profile/me
+//DESC - Get current user profile
+//ACCESS - Private
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate("user", ["firstName", "lastName"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "You do not have a profile." });
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error.");
+  }
+});
 
 module.exports = router;
