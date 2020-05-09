d8775c2d61faaace73cfcf3503af58af69977d6e
Web <web@Josss-MacBook-Pro.local>
2020-05-09 13:42:47 +0100

    Add/Remove comment functionality

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -123,9 +123,8 @@ router.put("/unlike/:id", auth, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     if (
-      (post.likes.filter(
-        (like) => like.user.toString() === req.user.id
-      ).length = 0)
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
     ) {
       return res.status(400).json({ msg: "Post has not yet been liked." });
     }
@@ -140,6 +139,72 @@ router.put("/unlike/:id", auth, async (req, res) => {
     res.json(post.likes);
   } catch (err) {
     console.error(err);
+    res.status(500).send("Server Error.");
+  }
+});
+
+//ROUTE - POST /api/posts/comment/:id
+//DESC - Comment on a post
+//ACCESS - Private
+router.post(
+  "/comment/:id",
+  [auth, [check("text", "Please enter some text.").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const post = await Post.findById(req.params.id);
+
+      const new_comment = {
+        text: req.body.text,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        user: req.user.id,
+      };
+
+      post.comments.unshift(new_comment);
+
+      await post.save();
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+//ROUTE - DELETE /api/posts/comment/:id/:comment_id
+//DESC - Delete comment on a post
+//ACCESS - Private
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+
+    if (!comment) {
+      return res.status(404).json({ msg: "Comment not found." });
+    }
+
+    if (comment.user.toString() != req.user.id) {
+      return res.status(401).json({ msg: "Unauthorised Request. " });
+    }
+
+    const remove_index = post.comments
+      .map((comment) => comment.user.toString())
+      .indexOf(req.user.id);
+
+    post.comments.splice(remove_index, 1);
+
+    await post.save();
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
     res.status(500).send("Server Error.");
   }
 });
