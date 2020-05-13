b13c58cef6026abb98390b00f54e9f92737650a6
Web <web@Josss-MacBook-Pro.local>
2020-05-13 18:22:44 +0100

    Login functionality added front end

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,6 +5,8 @@ import {
   REGISTER_FAIL,
   USER_LOADED,
   AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
 } from "./types";
 import setAuthToken from "../utils/setAuthToken";
 
@@ -42,6 +44,7 @@ export const register = ({ firstName, lastName, email, password }) => async (
       type: REGISTER_SUCCESS,
       payload: res.data,
     });
+    dispatch(loadUser());
   } catch (err) {
     const errors = err.response.data.errors;
     if (errors) {
@@ -49,6 +52,33 @@ export const register = ({ firstName, lastName, email, password }) => async (
     }
     dispatch({
       type: REGISTER_FAIL,
+    });
+  }
+};
+
+export const login = (email, password) => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-type": "application/json",
+    },
+  };
+
+  const body = JSON.stringify({ email, password });
+
+  try {
+    const res = await axios.post("/api/auth/", body, config);
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data,
+    });
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+    }
+    dispatch({
+      type: LOGIN_FAIL,
     });
   }
 };
