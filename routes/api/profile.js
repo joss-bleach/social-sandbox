0dd97d4c6950b34d80682487d58abfdda1279c8c
Web <web@Josss-MacBook-Pro.local>
2020-05-14 13:19:03 +0100

    Dashboard getCurrentProfile() error.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -17,11 +17,12 @@ router.get("/me", auth, async (req, res) => {
     }).populate("user", ["firstName", "lastName"]);
 
     if (!profile) {
-      return res.status(400).json({ msg: "You do not have a profile." });
+      return res.status(400).json({ msg: "There is no profile for this user" });
     }
+    res.json(profile);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server Error.");
+    res.status(500).send("Server Error");
   }
 });
 
