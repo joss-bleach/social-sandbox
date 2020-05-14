36a4bd01e8b1c25fa02655e8a09f69bb2b437073
Web <web@Josss-MacBook-Pro.local>
2020-05-14 19:52:49 +0100

    functionality to create and update profiles front end

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,13 +1,36 @@
-import React, { useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getCurrentProfile } from "../../actions/profile";
+import Spinner from "../layout/Spinner";
 
-const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+const Dashboard = ({
+  getCurrentProfile,
+  auth: { user },
+  profile: { profile, loading },
+}) => {
   useEffect(() => {
     getCurrentProfile();
   }, []);
-  return <div>Dashboard</div>;
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Welcome, {user && user.firstName}!
+      </p>
+      {profile !== null ? (
+        <Fragment>Has Profile</Fragment>
+      ) : (
+        <Fragment>
+          You haven't got a profile!{" "}
+          <Link to="/create-profile">Get Started</Link>
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
 Dashboard.propTypes = {
